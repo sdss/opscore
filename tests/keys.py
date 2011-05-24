4cb699c67b56d4b4d9851c215b6521328bc16525
dkirkby <dkirkby@pegasus.pa.uky.edu>
2011-05-24 03:45:13 +0000

    add OverflowError to list of exceptions caught when converting keyword values

--- tests/keys.py
+++ tests/keys.py
@@ -15,19 +15,23 @@ class KeysTest(unittest.TestCase):
         self.k1 = protoMess.Keyword('key1')
         self.k2 = protoMess.Keyword('key2',['-1.2'])
         self.k3 = protoMess.Keyword('key3',[0xdead,0xbeef])
+        self.k4 = protoMess.Keyword('key4',['?'])
         self.key1 = protoKeys.Key('Key1')
         self.key2 = protoKeys.Key('Key2', protoTypes.Float())
         self.key3 = protoKeys.Key('Key3', protoTypes.UInt()*2)
+        self.key4 = protoKeys.Key('Key4', protoTypes.Float(invalid='?'))
 
     def test00(self):
         "Key validation passes"
         self.failUnless(self.key1.consume(self.k1))
         self.failUnless(self.key2.consume(self.k2))
         self.failUnless(self.key3.consume(self.k3))
+        self.failUnless(self.key4.consume(self.k4))
         self.failUnless(len(self.k1.values) == 0)
         self.failUnless(self.k2.values[0] == -1.2)
         self.failUnless(self.k3.values[0] == 0xdead)
         self.failUnless(self.k3.values[1] == 0xbeef)
+        self.failUnless(self.k4.values[0] == None)
 
     def test01(self):
         "Key validation fails"
@@ -70,10 +74,11 @@ class KeysTest(unittest.TestCase):
         self.assertRaises(protoKeys.KeysError,lambda: self.key3.create('abc','0xdead'))
         
     def test07(self):
-        "Read testing dictionary"
-        kdict = protoKeys.KeysDictionary.load("testing")
-        self.failUnless('unsigned' in kdict)
-        self.failUnless('UnSigned' in kdict)
+        "Read testing dictionary (disabled since testing actor has been deleted)"
+        pass
+        #kdict = protoKeys.KeysDictionary.load("testing")
+        #self.failUnless('unsigned' in kdict)
+        #self.failUnless('UnSigned' in kdict)
         
     def test08(self):
         "Generic compound value type without explicit wrapper"
@@ -126,6 +131,14 @@ class KeysTest(unittest.TestCase):
         self.failUnless(isinstance(msg.values[0],RO.PVT.PVT))
         self.assertEqual(repr(msg.values[0]),repr(RO.PVT.PVT(1,2,3)))
         self.assertEqual(msg.values[1],4)
+    
+    def test12(self):
+        "Invalid value"
+        self.key4.consume(self.k4)
+        ival = self.k4.values[0]
+        self.assertEqual(ival,None)
+        self.assertEqual(ival,protoTypes.InvalidValue)
+        self.assertEqual({None:'-None-',protoTypes.InvalidValue:'-Invalid-'}[ival],'-Invalid-')
         
 if __name__ == '__main__':
     unittest.main()
