09b90a6a388e19350aeeb40368714a8d83abc90d
rowen <rowen@pegasus.pa.uky.edu>
2012-07-24 23:20:58 +0000

    Minor cleanups

--- python/opscore/actor/actordispatcher.py
+++ python/opscore/actor/actordispatcher.py
@@ -1,32 +1,20 @@
-#!/usr/bin/env python
 """A single-actor version of CmdKeyDispatcher.
 
 Send commands to a single actor and dispatches replies from that actor.
 
 History:
-2012-07-24 ROwen    first cut
+2012-07-24 ROwen
 """
 import sys
-import time
 import traceback
 
-import RO.Alg
 import RO.Constants
-import RO.StringUtil
-
-from opscore.utility.timer import Timer
-import opscore.protocols.keys as protoKeys
 from opscore.protocols.parser import ActorReplyParser
-import opscore.protocols.messages as protoMess
-import keydispatcher
-import keyvar
+from .keyvar import MsgCodeSeverity
+from .keydispatcher import KeyVarDispatcher
 from .cmdkeydispatcher import CmdKeyVarDispatcher
 
 __all__ = ["ActorDispatcher"]
-
-_CmdNumWrap = 1000 # value at which user command ID numbers wrap
-
-_RefreshTimeLim = 20 # time limit for refresh commands (sec)
 
 class ActorDispatcher(CmdKeyVarDispatcher):
     """Parse replies and sets KeyVars. Also manage CmdVars and their replies.
@@ -81,7 +69,7 @@ class ActorDispatcher(CmdKeyVarDispatcher):
 #        print "%s.addKeyVar(%s); hasRefreshCmd=%s; refreshInfo=%s" % (self.__class__.__name__, keyVar, keyVar.hasRefreshCmd, keyVar.refreshInfo)
         if keyVar.actor != self.name:
             raise RuntimeError("keyVar.actor=%r; this actor dispatcher only handles actor %r" % (keyVar.actor, self.name))
-        keydispatcher.KeyVarDispatcher.addKeyVar(self, keyVar)
+        KeyVarDispatcher.addKeyVar(self, keyVar)
 
     def logReply(self, reply, fallbackToStdOut = False):
         """Log a reply (an opscore.protocols.messages.Reply)
@@ -99,7 +87,7 @@ class ActorDispatcher(CmdKeyVarDispatcher):
         """
         try:
             msgCode = reply.header.code
-            severity = keyvar.MsgCodeSeverity[msgCode]
+            severity = MsgCodeSeverity[msgCode]
             self.logMsg(
                 msgStr = reply.string,
                 severity = severity,
@@ -167,86 +155,3 @@ class ActorDispatcher(CmdKeyVarDispatcher):
         """Format a reply header; one-actor version
         """
         return "%d %d %s" % (cmdID, self._myUserID, msgCode)
-
-
-if __name__ == "__main__":
-    print "\nTesting opscore.actor.ActorDispatcher\n"
-    import opscore.protocols.types as protoTypes
-    import twisted.internet.tksupport
-    import Tkinter
-    root = Tkinter.Tk()
-    twisted.internet.tksupport.install(root)
-    
-    kvd = ActorDispatcher()
-
-    def showVal(keyVar):
-        print "keyVar %s.%s = %r, isCurrent = %s" % (keyVar.actor, keyVar.name, keyVar.valueList, keyVar.isCurrent)
-
-    # scalars
-    keyList = (
-        protoKeys.Key("StringKey", protoTypes.String()),
-        protoKeys.Key("IntKey", protoTypes.Int()),
-        protoKeys.Key("FloatKey", protoTypes.Float()),
-        protoKeys.Key("BooleanKey", protoTypes.Bool("F", "T")),
-        protoKeys.Key("KeyList", protoTypes.String(), protoTypes.Int()),
-    )
-    keyVarList = [keyvar.KeyVar("test", key) for key in keyList]
-    for keyVar in keyVarList:
-        keyVar.addCallback(showVal)
-        kvd.addKeyVar(keyVar)
-    
-    # command callback
-    def cmdCall(cmdVar):
-        print "command callback for actor=%s, cmdID=%d, cmdStr=%r, isDone=%s" % \
-            (cmdVar.actor, cmdVar.cmdID, cmdVar.cmdStr, cmdVar.isDone)
-    
-    # command
-    cmdVar = keyvar.CmdVar(
-        cmdStr = "THIS IS A SAMPLE COMMAND",
-        actor="test",
-        callFunc=cmdCall,
-        callCodes = keyvar.DoneCodes,
-    )
-    kvd.executeCmd(cmdVar)
-    cmdID = cmdVar.cmdID
-
-    dataList = [
-        "StringKey=hello",
-        "IntKey=1",
-        "FloatKey=1.23456789",
-        "BooleanKey=T",
-        "KeyList=three, 3",
-        "Coord2Key=45.0, 0.1, 32.1, -0.1, %s" % (time.time(),),
-    ]
-    dataStr = "; ".join(dataList)
-
-    reply = kvd.makeReply(
-        cmdr = "myprog.me",
-        cmdID = cmdID - 1,
-        actor = "test",
-        msgCode = ":",
-        dataStr = dataStr,
-    )
-    print "\nDispatching message with wrong cmdID; only KeyVar callbacks should called:"
-    kvd.dispatchReply(reply)
-
-    reply = kvd.makeReply(
-        cmdID = cmdID,
-        actor = "wrongActor",
-        msgCode = ":",
-        dataStr = dataStr,
-    )
-    print "\nDispatching message with wrong actor; only CmdVar callbacks should be called:"
-    kvd.dispatchReply(reply)
-
-    reply = kvd.makeReply(
-        cmdID = cmdID,
-        actor = "test",
-        msgCode = ":",
-        dataStr = dataStr,
-    )
-    print "\nDispatching message correctly; CmdVar done so only KeyVar callbacks should be called:"
-    kvd.dispatchReply(reply)
-    
-    print "\nTesting keyVar refresh"
-    kvd.refreshAllVar()
