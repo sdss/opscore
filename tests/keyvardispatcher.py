363def77c239af5f8e21332540ee497170c2d857
rowen <rowen@pegasus.pa.uky.edu>
2015-09-28 17:03:17 +0000

    Enhance tests/keyvardispatcher to test unwrapping of values to native types

--- tests/keyvardispatcher.py
+++ tests/keyvardispatcher.py
@@ -11,53 +11,70 @@ except ImportError:
     # older version of RO
     pass
 from opscore.actor import Model, KeyVarDispatcher
+from RO.PVT import PVT
 
 Model.setDispatcher(KeyVarDispatcher())
-HubModel = Model("hub")
+TCCModel = Model("tcc")
 
 class ModelTests(unittest.TestCase):
-    model = HubModel
+    model = TCCModel
     dispatcher = model.dispatcher
 
     def testDispatch(self):
         """Test dispatching"""
         # keywords are initialized to empty tuples or tuples of None
-        for keyName in ("actors", "commanders", "user", "users", "version", "httpRoot"):
+        for keyName in ("axisConnState", "userNum", "version", "ut1"):
             keyVar = getattr(self.model, keyName)
             self.assertEquals(keyVar[:], (None,)*len(keyVar))
 
-        replyStr = self.makeReplyStr(actor="badactor", dataStr="actors=calvin,hobbes; commanders=tu01.mice,tu02.men; users=anon,you,me; version=1.0; httpRoot=hub25m.apo, image/dir")
+        replyStr = self.makeReplyStr(actor="badactor", dataStr="userNum=5; version=1.0")
         self.dispatcher.dispatchReplyStr(replyStr)
-        for keyName in ("actors", "commanders", "user", "users", "version", "httpRoot"):
+        for keyName in ("userNum", "version"):
             keyVar = getattr(self.model, keyName)
             self.assertEquals(keyVar[:], (None,)*len(keyVar))
         
-        replyStr = self.makeReplyStr(actor="hub", dataStr="actors=calvin,hobbes; commanders=tu01.mice,tu02.men; users=anon,you,me; version=1.0; httpRoot=hub25m.apo, image/dir")
+        replyStr = self.makeReplyStr(actor="tcc",
+            dataStr="tccPos=123.4, 45.6, -23.4; userNum=5; version=1.0; convAng=1.1, 0.22, 1234.5")
         self.dispatcher.dispatchReplyStr(replyStr)
-        self.assertEquals(self.model.actors[:], ("calvin", "hobbes"))
-        self.assertEquals(self.model.commanders[:], ("tu01.mice", "tu02.men"))
-        self.assertEquals(self.model.users[:], ("anon", "you", "me"))
-        self.assertEquals(self.model.version[:], ("1.0",))
-        self.assertEquals(self.model.httpRoot[:], ("hub25m.apo", "image/dir"))
-    
+        for i in range(3):
+            self.assertAlmostEquals(self.model.tccPos[i], (123.4, 45.6, -23.4)[i])
+            self.assertEquals(type(self.model.tccPos[i]), float)
+        self.assertEquals(self.model.userNum[0], 5)
+        self.assertEquals(type(self.model.userNum[0]), long)
+        self.assertEquals(self.model.version[0], "1.0")
+        self.assertEquals(type(self.model.version[0]), str)
+        convAng = self.model.convAng[0]
+        self.assertAlmostEquals(convAng.pos, 1.1)
+        self.assertAlmostEquals(convAng.vel, 0.22)
+        self.assertAlmostEquals(convAng.t,   1234.5)
+        self.assertEquals(type(convAng), PVT)
+
+    def testInvalidValues(self):
+        """Test that invalid values map to None (but invalid PVTs map to invalid PVTs)
+        """
+        replyStr = self.makeReplyStr(actor="tcc", dataStr="tccPos=nan, nan, nan; convAng=1.1, 0.22, nan")
+        self.dispatcher.dispatchReplyStr(replyStr)
+        for i in range(3):
+            self.assertTrue(self.model.tccPos[i] is None)
+        self.assertFalse(self.model.convAng[0].isValid())
+
     def testGetKeyVarList(self):
-        for keyName in ("actors", "commanders", "user", "users", "version", "httpRoot"):
-            keyVarList = self.dispatcher.getKeyVarList("hub", keyName)
+        for keyName in ("userNum", "version", "convAng"):
+            keyVarList = self.dispatcher.getKeyVarList("tcc", keyName)
             self.assertEqual(len(keyVarList), 1)
             self.assertEqual(keyVarList[0], getattr(self.model, keyName))
         self.assertEquals(self.dispatcher.getKeyVarList("badactorname", "users"), [])
-        self.assertEquals(self.dispatcher.getKeyVarList("hub", "badkeyvarname"), [])
+        self.assertEquals(self.dispatcher.getKeyVarList("tcc", "badkeyvarname"), [])
     
     def testGetKeyVar(self):
-        self.assertTrue(self.model.actors is self.dispatcher.getKeyVar("hub", "actors"))
-        for keyName in ("actors", "commanders", "user", "users", "version", "httpRoot"):
-            keyVar = self.dispatcher.getKeyVar("hub", keyName)
+        for keyName in ("convAng", "userNum", "version"):
+            keyVar = self.dispatcher.getKeyVar("tcc", keyName)
             self.assertEqual(keyVar, getattr(self.model, keyName))
         self.assertRaises(Exception, self.dispatcher.getKeyVar, "badactorname", "users")
-        self.assertRaises(Exception, self.dispatcher.getKeyVar, "hub", "badkeyvarname")
+        self.assertRaises(Exception, self.dispatcher.getKeyVar, "tcc", "badkeyvarname")
         self.assertRaises(Exception, self.dispatcher.getKeyVar, "nonexistentKeyVar")
     
-    def makeReplyStr(self, dataStr, cmdr="me.me", cmdID=0, actor="hub"):
+    def makeReplyStr(self, dataStr, cmdr="me.me", cmdID=0, actor="tcc"):
         return "%s %d %s : %s" % (cmdr, cmdID, actor, dataStr)
 
 if __name__ == "__main__":
